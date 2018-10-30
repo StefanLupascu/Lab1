9e57fb52bc85935a8e666c47a2fb2736a2eda78c
Stefan Lupascu <lupascu_stefan97@yahoo.com>
2018-10-30 09:38:37 +0200

    Some changes

--- Lab1/ViewController.swift
+++ Lab1/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let versionLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .gray
         print("Hello world!")
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.addSubview(versionLabel)
+        
+        versionLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        versionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+//        guard let revisionNumber = Bundle.main.releaseVersionNumber else {
+//            return
+//        }
+        let revisionNumber = Bundle.main.releaseVersionNumberPretty
+        versionLabel.translatesAutoresizingMaskIntoConstraints = false
+        versionLabel.text = "Revision number: \(revisionNumber)"
     }
 }
 
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+    var releaseVersionNumberPretty: String {
+        return "v\(releaseVersionNumber ?? "1.0.0")"
+    }
+}
